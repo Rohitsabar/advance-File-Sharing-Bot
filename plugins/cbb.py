74c46bb680c8cc789d3117513d11e2057a0eabbc
Ybot827 <98046884+Ybot827@users.noreply.github.com>
2022-04-23 10:37:23 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : Unknown</a>\n○ Channel : Unkown\n○ Support Group : Unkown</b>",
+            text = f"<b>○ Creator : Unknown</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : Unknown</a>\n○ Channel : Unkown\n○ Support Group : Unkown</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
